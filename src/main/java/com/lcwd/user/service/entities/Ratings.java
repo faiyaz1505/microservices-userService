e59ddb4775f7fbb08b47f425b358de09a7a52c5a
faiyaz1505 <fasc786@gmail.com>
2023-11-09 19:27:17 +0530

    first push

---
+++ src/main/java/com/lcwd/user/service/entities/Ratings.java
@@ -0,0 +1,18 @@
+package com.lcwd.user.service.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ratings {
+
+    private String ratingId;
+    private String userId;
+    private String hotelId;
+    private int rating;
+    private String feedback;
+}
---
+++ src/main/java/com/lcwd/user/service/entities/User.java
@@ -0,0 +1,32 @@
+package com.lcwd.user.service.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "micro_users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @Column(name = "ID")
+    private String userId;
+    @Column(name = "NAME",length = 20)
+    private String name;
+    @Column(name = "EMAIL")
+    private String email;
+    @Column(name = "ABOUT")
+    private String about;
+
+    @Transient
+    private List<Ratings> ratings=new ArrayList<>();
+}
